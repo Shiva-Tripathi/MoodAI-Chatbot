15009452ce0cb8179c9f024c39fb087cd61d869f
Shiva-Tripathi <88522633+Shiva-Tripathi@users.noreply.github.com>
2026-03-20 15:58:36 +0530

    Add files via upload

---
+++ huggingface.py
@@ -0,0 +1,14 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id= "deepseek-ai/DeepSeek-R1"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke("who are you?")
+
+print(response.content)
